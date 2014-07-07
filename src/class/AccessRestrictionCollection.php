e5d8f0a5737692b97de1a2d34e6302e1e57b69b7
Rüdiger Scheumann <ruediger.scheumann@web.de>
2014-07-07 21:53:47 +0200

    * fix

--- src/class/AccessRestrictionCollection.php
+++ src/class/AccessRestrictionCollection.php
@@ -8,6 +8,6 @@ class AccessRestrictionCollection extends GenericCollection
 {
     public function __construct()
     {
-        parent::__construct('De\Welt\JobPortal\AccessRestriction');
+        parent::__construct('Com\PaulDevelop\Library\Auth\AccessRestriction');
     }
 }
